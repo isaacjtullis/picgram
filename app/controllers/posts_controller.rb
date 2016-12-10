343d3e96190d7f91475637983beb0d56745129fc
Isaac Tullis <isaacjtullis@gmail.com>
2016-12-09 18:20:22 -0700

    added paperclip for users to add photos

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 class PostsController < ApplicationController
   def index
+    @new.all
+  end
+
+  def new
+    @new = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image)
   end
 end
