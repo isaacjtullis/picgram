c365e5ef5ebd96e40e240077a3b80be924ca35ed
Isaac Tullis <isaacjtullis@gmail.com>
2016-12-09 18:38:32 -0700

    Installed simple_form, refactored posts new, create and index controller, displayed photos with image_tag

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,19 @@
 class PostsController < ApplicationController
   def index
-    @new.all
+    @posts = Post.all
   end
 
   def new
-    @new = Post.new
+    @post = Post.new
   end
 
   def create
     @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   private
